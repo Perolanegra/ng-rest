073fe06f7a50f6e329a16094bf9ed4142fa42302
perolanegra <igoralves@devbaiano.com.br>
2020-05-24 12:35:48 -0300

    Adicionado Cors

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,16 @@ import { AppModule } from './app.module';
 import "reflect-metadata";
 import * as helmet from 'helmet';
 import * as rateLimit from 'express-rate-limit';
+const cors = require('cors');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
+  const corsOptions = {
+    origin: '*',
+    optionsSucessStatus: 200
+  };
+  app.use(cors(corsOptions));
   app.use(helmet());
 
   app.use(
