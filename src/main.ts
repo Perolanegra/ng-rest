9de657b04f4303d54084ef833db1cc08b6470210
perolanegra <igoralves@devbaiano.com.br>
2020-05-26 16:53:07 -0300

    Voltar versao

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 import "reflect-metadata";
 import * as helmet from 'helmet';
 import * as rateLimit from 'express-rate-limit';
-import * as proxy from 'http-proxy-middleware';
 const cors = require('cors');
 
 async function bootstrap() {
@@ -15,22 +14,6 @@ async function bootstrap() {
   };
   app.use(cors(corsOptions));
   app.use(helmet());
-
-  app.use(
-    "https://perolanegra.github.io/api/*",
-    proxy.createProxyMiddleware({
-      target: process.env.API,
-      // pathRewrite: {
-      //   'https://perolanegra.github.io/api/': ''
-      // },
-      secure: true,
-      onProxyReq: (proxyReq, req, res) => {
-        console.log(
-          `[Global Functional Middlware]: Proxying ${req.method} request originally made to '${req.originalUrl}'...`
-        );
-      }
-    })
-  );
 
   app.use(
     rateLimit({
