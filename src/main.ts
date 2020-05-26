fa2152317fc02d6398e10b0409408573b69c8cfb
perolanegra <igoralves@devbaiano.com.br>
2020-05-26 16:35:09 -0300

    Ajustes ProxyConfig e att versao

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,11 @@ import { AppModule } from './app.module';
 import "reflect-metadata";
 import * as helmet from 'helmet';
 import * as rateLimit from 'express-rate-limit';
+import * as proxy from 'http-proxy-middleware';
 const cors = require('cors');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bodyParser: false });
   const port = process.env.PORT || 3000;
   const corsOptions = {
     origin: '*',
@@ -14,6 +15,22 @@ async function bootstrap() {
   };
   app.use(cors(corsOptions));
   app.use(helmet());
+
+  app.use(
+    "https://perolanegra.github.io/api/*",
+    proxy.createProxyMiddleware({
+      target: process.env.API,
+      // pathRewrite: {
+      //   'https://perolanegra.github.io/api/': ''
+      // },
+      secure: true,
+      onProxyReq: (proxyReq, req, res) => {
+        console.log(
+          `[Global Functional Middlware]: Proxying ${req.method} request originally made to '${req.originalUrl}'...`
+        );
+      }
+    })
+  );
 
   app.use(
     rateLimit({
