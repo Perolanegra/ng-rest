89b87ed557b4ee9b36ba30fc6904e007ada9a0a7
perolanegra <igoralves@devbaiano.com.br>
2020-06-05 14:06:47 -0300

    OK

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,8 @@ import { UsersModule } from './user/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MailerModule, HandlebarsAdapter } from '@nestjs-modules/mailer';
 import { IssuesModule } from './issues/issues.module';
-import { MediaModule } from './media/media.module';
-import { PhotosModule } from './photos/photos.module';
-import { AnswersModule } from './answers/answers.module';
+import { PostModule } from './post/post.module';
 import { TagsModule } from './tags/tags.module';
-
 
 @Module({
   imports: [
@@ -17,11 +14,10 @@ import { TagsModule } from './tags/tags.module';
     UsersModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.HOST || 'localhost',
-      port: 3306,
-      username: process.env.USERNAME || 'ng',
-      password: process.env.PASSWORD || '1061043$',
-      database: process.env.DATABASE || 'ng_ba',
+      host: process.env.HOST,
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       acquireTimeout: 60 * 60 * 1000,
       connectTimeout: 60 * 60 * 1000,
@@ -31,14 +27,14 @@ import { TagsModule } from './tags/tags.module';
     MailerModule.forRoot({
       transport: {
         service: 'gmail',
-        host: process.env.SMTP_HOST || 'smtp.gmail.com',
+        host: process.env.SMTP_HOST,
         port: parseInt(process.env.SMTP_PORT, 10) || 465,
         ssl: true,
-        secure: true,//process.env.SMTP_SECURE === 'true', // true for 465, false for other ports
+        secure: true, //process.env.SMTP_SECURE === 'true', // true for 465, false for other ports
         ignoreTLS: process.env.SMTP_SECURE !== 'false',
         auth: {
-          user: process.env.SMTP_AUTH_USER || 'pedratto3@gmail.com',
-          pass: process.env.SMTP_AUTH_PASS || 'ixcjtcqeavaoelmh',
+          user: process.env.SMTP_AUTH_USER,
+          pass: process.env.SMTP_AUTH_PASS,
         },
       },
       defaults: {
@@ -53,11 +49,9 @@ import { TagsModule } from './tags/tags.module';
       },
     }),
     IssuesModule,
-    MediaModule,
-    PhotosModule,
-    AnswersModule,
+    PostModule,
     TagsModule
   ],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
