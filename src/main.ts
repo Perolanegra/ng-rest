a512f18f5650f23c333a4b9dfaa412394348ea75
perolanegra <igoralves@devbaiano.com.br>
2020-06-05 17:46:31 -0300

    Ahjuste cors2

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,22 @@ import { AppModule } from './app.module';
 import "reflect-metadata";
 import * as helmet from 'helmet';
 import * as rateLimit from 'express-rate-limit';
-// const cors = require('cors');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
   app.use(helmet());
-  app.enableCors({ origin: 'https://www.ng-forum.com/' })
-  // var corsOptions = {
-  //   origin: 'https://www.ng-forum.com',
-  //   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  // }
-  // app.use(cors(corsOptions))
-  // 
+
+  const options = {
+    origin: 'https://www.ng-forum.com/',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: true,
+    optionsSuccessStatus: 200,
+    credentials: true,
+  };
+
+  app.enableCors(options);
+ 
   app.use(
     rateLimit({
       windowMs: 1 * 60 * 1000, // 1 minutes
