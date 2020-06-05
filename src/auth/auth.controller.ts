3f873363f19a01df6b7718630093249a1776608e
perolanegra <igoralves@devbaiano.com.br>
2020-06-05 14:08:34 -0300

    Ajuste CORS

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 
-import { Controller, Request, Post, UseGuards, Get, Param, Body } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Get, Body } from '@nestjs/common';
 import { LocalAuthGuard } from './local-auth.guard';
 import { AuthService } from './auth.service';
 import { AppService } from '../app.service';
